9558a0483473c8b2e19a89f4e84e92a8ffb7183b
Zffu <radi0o0@outlook.com>
2024-03-04 07:09:01 +0100

    - Added Javadoc on WrappedPosition

--- generic/src/main/java/net/zffu/crystal/wrappers/WrappedPosition.java
+++ generic/src/main/java/net/zffu/crystal/wrappers/WrappedPosition.java
@@ -5,11 +5,34 @@ package net.zffu.crystal.wrappers;
  */
 public interface WrappedPosition {
 
+    /**
+     * Gets the Position's X value.
+     * @return the value.
+     */
     double getX();
+
+    /**
+     * Gets the Position's Y value.
+     * @return the value.
+     */
     double getY();
+
+    /**
+     * Gets the Position's Z value.
+     * @return the value.
+     */
     double getZ();
 
+    /**
+     * Ges the Position's Yaw
+     * @return the yaw
+     */
     float getYaw();
+
+    /**
+     * Gets the Position's Pitch
+     * @return the pitch.
+     */
     float getPitch();
 
 }
