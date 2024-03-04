66dd6d300ab9fae2c6017f8cf0eab1988e0a9540
Zffu <radi0o0@outlook.com>
2024-03-04 07:08:42 +0100

    - Added start(WrappedPosition) function - Added onStart() function - Added startNextSequence(WrappedRunnable) function - Added stopSequence() function - Added lastPosition variable

--- generic/src/main/java/net/zffu/crystal/sequences/Sequence.java
+++ generic/src/main/java/net/zffu/crystal/sequences/Sequence.java
@@ -1,14 +1,15 @@
 package net.zffu.crystal.sequences;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.Setter;
-
-import java.util.HashMap;
+import net.zffu.crystal.wrappers.WrappedPosition;
+import net.zffu.crystal.wrappers.task.WrappedRunnable;
 
 /**
  * Represents a simple animation sequence. A Sequence is an animation series of stuff happening on the player's view.
  */
-public class Sequence {
+public abstract class Sequence {
 
     /**
      * Time (in ticks) before triggering the next frame.
@@ -16,6 +17,37 @@ public class Sequence {
     @Setter(AccessLevel.PROTECTED)
     private int ticksPerFrame = 20;
 
+    /**
+     * Last position that was used in the {@link Sequence}.
+     */
+    @Getter(AccessLevel.PROTECTED)
+    @Setter(AccessLevel.PROTECTED)
+    private WrappedPosition lastPosition;
 
+    /**
+     * Starts playing the current {@link Sequence} instance.
+     * @param position
+     */
+    public final void start(WrappedPosition position) {
+        this.lastPosition = position;
+    }
+
+    /**
+     * Called automatically on sequence start, you should start your animation in that function.
+     */
+    protected abstract void onStart();
+
+    /**
+     * Starts the next sequence based on a {@link WrappedRunnable} for the execution.
+     * @param runnable
+     */
+    protected final void startNextSequence(WrappedRunnable runnable) {
+        //todo: add core task wrapper
+    }
+
+    /**
+     * Stops the {@link Sequence} instance.
+     */
+    public void stopSequence() {}
 
 }
