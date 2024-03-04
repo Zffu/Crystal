6f84bca028de2a655b087f84bf07538aad585b6d
Zffu <radi0o0@outlook.com>
2024-03-04 07:09:14 +0100

    - Created Wrapped Runnable

---
+++ generic/src/main/java/net/zffu/crystal/wrappers/task/WrappedRunnable.java
@@ -0,0 +1,13 @@
+package net.zffu.crystal.wrappers.task;
+
+/**
+ * Wrapped class for a runnable.
+ */
+public interface WrappedRunnable {
+
+    /**
+     * Runs the {@link WrappedRunnable}.
+     */
+    void run();
+
+}
